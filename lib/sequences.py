a46932b2be87606ab8ddb7b3829143096087063e
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-28 00:42:51 -0400

    passing tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-sequence = []
-
 def print_fibonacci(length):
+    sequence = []
     for i in range(length):
         if i > 1:
             num = sequence[-2] + sequence[-1]
         else:
             num = i
         sequence.append(num)
+    print(sequence)
 
-# print_fibonacci(0)
+# print_fibonacci(4)
 # print(sequence)
